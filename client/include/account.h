953a0b73dfddd04da4ae3817e409993c69d0af31
aducne2512 <anhnguyenduc750@gmail.com>
2025-11-25 22:10:58 +0700

    updddd

--- client/include/account.h
+++ client/include/account.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "table.h"
+#include "dbConnection.h"
+#include <string>
 
 class AccountTable : public Table {
 public:
@@ -9,4 +11,11 @@ public:
 
     ssize_t setAccount(const std::string& username, const std::string& host, const std::string& port);
     AccountState getLastAccount();
+    // [THÊM MỚI] --- Chức năng Smart Login ---
+    bool createHostsTableIfNeeded(); // Tạo bảng hosts (ip, hostname)
+    void saveHost(const std::string& hostname, const std::string& ip); // Lưu cặp Host-IP
+    std::string getHostname(const std::string& ip); // Tra cứu Hostname từ IP
+    // ----------------------------------------
+
+
 };
